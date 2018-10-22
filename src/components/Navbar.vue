1ea8f9f5755f92d40c57994d3220da5a0ebd0682
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2018-10-22 06:57:45 +0530

    Navbar Fixed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class="navbar navbar-default navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <router-link class="navbar-brand" to="/">Photo Share</router-link>
+      <router-link class="navbar-brand text-primary" to="/"><b><h2>Photo Share</h2></b></router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
         aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -11,30 +11,32 @@
         </ul>
         <ul class="nav navbar-nav navbar-left padding">
           <li v-if="isVerefied" class="nav-item">
-            <router-link class="nav-link p-2" to="/" aria-label="Home" data-toggle="tooltip" data-placement="bottom" title="Home"><i class="fa fa-home fa-lg"></i></router-link>
+            <router-link class="nav-link p-2" to="/" aria-label="Home" data-toggle="tooltip" data-placement="bottom" title="Home"><i class="fa fa-home fa-lg text-info"></i></router-link>
           </li>
           <li v-if="isVerefied" class="nav-item ">
-            <router-link class="nav-link p-2" to="/Notification" aria-label="Notification" data-toggle="tooltip" data-placement="bottom" title="Notification"><i class="fa fa-bell fa-lg"></i></router-link>
+            <router-link class="nav-link p-2" to="/Notification" aria-label="Notification" data-toggle="tooltip" data-placement="bottom" title="Notification"><i class="fa fa-bell fa-lg text-info"></i></router-link>
           </li>
           <li v-if="isVerefied" lass="nav-item ">
-            <router-link class="nav-link p-2" to="/Trends" aria-label="Trends" data-toggle="tooltip" data-placement="bottom" title="Trends"><i class="fa fa-fire fa-lg"></i></router-link>
+            <router-link class="nav-link p-2" to="/Trends" aria-label="Trends" data-toggle="tooltip" data-placement="bottom" title="Trends"><i class="fa fa-fire fa-lg text-info"></i></router-link>
           </li>
           <li v-if="isVerefied" class="nav-item ">
-            <router-link class="nav-link p-2" to="/Friends" aria-label="Friends" data-toggle="tooltip" data-placement="bottom" title="Friends"><i class="fa fa-user-friends fa-lg"></i></router-link>
+            <router-link class="nav-link p-2" to="/Friends" aria-label="Friends" data-toggle="tooltip" data-placement="bottom" title="Friends"><i class="fa fa-user-friends fa-lg text-info"></i></router-link>
           </li>
           <li v-if="isVerefied" class="nav-item ">
-            <router-link class="nav-link p-2" to="/Messages" aria-label="Messages" data-toggle="tooltip" data-placement="bottom" title="Messages"><i class="fa fa-comments fa-lg"></i></router-link>
+            <router-link class="nav-link p-2" to="/Messages" aria-label="Messages" data-toggle="tooltip" data-placement="bottom" title="Messages"><i class="fa fa-comments fa-lg text-info"></i></router-link>
           </li>
           <li v-if="isVerefied" class="nav-item ">
-            <router-link class="nav-link p-2" to="/Account" aria-label="Account" data-toggle="tooltip" data-placement="bottom" title="Account"><i class="fa fa-user-alt fa-lg"></i></router-link>
+            <router-link class="nav-link p-2" to="/Account" aria-label="Account" data-toggle="tooltip" data-placement="bottom" title="Account"><i class="fa fa-user-alt fa-lg text-info"></i></router-link>
           </li>
             <div v-if="isVerefied" class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>User<span class="caret"></span></a>
+            <div class="container">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{isLogedIn}}<span class="caret"></span></a>
             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                 <router-link class="dropdown-item" to="/Setting"><i class="fa fa-cog"></i> Setting</router-link>
                 <router-link class="dropdown-item" to="/Stories"><i class="fa fa-plus"></i> Stories</router-link>
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" v-on:click="logout">Logout</a>
+            </div>
             </div>
             </div>
           <li v-if="!isLogedIn" class="nav-item ">
@@ -43,7 +45,7 @@
           <li v-if="!isLogedIn" class="nav-item ">
             <router-link class="nav-link p-2" to="/Signup" aria-label="SIGNUP" data-toggle="tooltip" data-placement="bottom" title="SIGNUP">SIGNUP</router-link>
           </li>
-          <li v-if="isLogedIn" class="nav-item ">
+          <li v-if="isLogedInNotVerefied" class="nav-item ">
             <button v-on:click="logout" class="nav-link p-2" aria-label="LOGOUT" data-toggle="tooltip" data-placement="bottom" title="LOGOUT">LOGOUT</button>
           </li>
         </ul>
@@ -60,12 +62,15 @@ export default {
   data() {
     return {
       isLogedIn: false,
-      isVerefied: false
+      isVerefied: false,
+      isLogedInNotVerefied: false,
+      currentUser: false
     }
   },
   created() {
     if(firebase.auth().currentUser) {
       this.isLogedIn = true
+      this.isLogedInNotVerefied =true
       this.currentUser = firebase.auth().currentUser.email
       firebase.auth().onAuthStateChanged(user => {
         if(!user.emailVerified) {
@@ -73,6 +78,7 @@ export default {
         }
         else {
           this.isVerefied = true
+          this.isLogedInNotVerefied = false
         }
       })
       
