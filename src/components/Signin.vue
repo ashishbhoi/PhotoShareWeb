7ae3eef60e53e962f0c1b33392dac6e1593313a3
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2018-10-22 18:21:48 +0530

    Now You Can Signin With Gmail And Facebook

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -29,6 +29,13 @@
     <button type="submit" class="btn btn-primary" v-on:click="Signin">Login</button>
     <router-link class="btn btn-link" to="/ForgotPassword">Forgot Your Password?</router-link>
     </div>
+    <br>
+    <br>
+    <div class="col-md-8 offset-md-4 text-md-right fa-2x text-info">
+        <a v-on:click="GoogleLogin"> <i class="fab fa-google"></i></a> 
+        {{"       "}}
+        <a  v-on:click="FacebookLogin"> <i class="fab fa-facebook"></i></a>
+    </div>
     </div>
     </form>
 </div>
@@ -42,6 +49,9 @@
 <script>
 import firebase from 'firebase/app'
 import 'firebase/auth'
+var provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+var providerFacebook = new firebase.auth.FacebookAuthProvider();
 export default {
     name: 'signin',
     data: function() {
@@ -53,6 +63,30 @@ export default {
     methods: {
         Signin : function(e) {
             firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+                user => {
+                    alert('You Are logged in')
+                    this.$router.go({path: this.$router.path})
+                },
+                err => {
+                    alert(err.message)
+                }
+            )
+            e.preventDefault()
+        },
+        GoogleLogin: function(e) {
+            firebase.auth().signInWithPopup(provider).then(
+                user => {
+                    alert('You Are logged in')
+                    this.$router.go({path: this.$router.path})
+                },
+                err => {
+                    alert(err.message)
+                }
+            )
+            e.preventDefault()
+        },
+        FacebookLogin: function(e) {
+            firebase.auth().signInWithPopup(providerFacebook).then(
                 user => {
                     alert('You Are logged in')
                     this.$router.go({path: this.$router.path})
